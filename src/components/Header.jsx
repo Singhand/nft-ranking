5acacdab1180992257c0b54e1b5bb2be985a08fc
KSH <rla_tmdgus@ssafy.com>
2023-12-15 04:26:24 +0900

    feat: Header component (메인, 디테일, 마이페이지)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,111 @@
 import styled from "styled-components";
+import { buttonBorderRadius, buttonColor, textColor } from "../style/common";
+import { Button } from "./Button";
+
+const HEIGHT = "50px";
+
+const Title = styled.div`
+  ${textColor}
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+const IconBtn = styled(Button)`
+  width: ${HEIGHT};
+  height: ${HEIGHT};
+`;
+
+const HeaderCtn = styled.div`
+  width: 100%;
+  height: ${HEIGHT};
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+`;
+
+const IconBtnRight = styled(IconBtn)`
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+
+const IconBtnLeft = styled(IconBtn)`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export function MainHeader({ click }) {
+  return (
+    <HeaderCtn>
+      <Title>NFT</Title>
+      <IconBtnRight onClick={click}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </IconBtnRight>
+    </HeaderCtn>
+  );
+}
+
+export function DetailHeader({ click }) {
+  return (
+    <HeaderCtn>
+      <IconBtnLeft onClick={click}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"
+          />
+        </svg>
+      </IconBtnLeft>
+    </HeaderCtn>
+  );
+}
+
+export function MpHeader({ click }) {
+  // my page header
+  return (
+    <HeaderCtn>
+      <Title>MY</Title>
+      <IconBtnLeft onClick={click}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"
+          />
+        </svg>
+      </IconBtnLeft>
+    </HeaderCtn>
+  );
+}
