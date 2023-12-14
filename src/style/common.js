942a9e298ea9d92edb18757c0bcf7eb8f030e196
KSH <rla_tmdgus@ssafy.com>
2023-12-15 04:00:51 +0900

    common css

---
+++ src/style/common.js
@@ -0,0 +1,29 @@
+import styled, { css } from "styled-components";
+
+export const backgroundColor = css`
+  background-color: #070707;
+`;
+
+export const textColor = css`
+  color: #fff;
+`;
+
+export const buttonColor = css`
+  background-color: #1f1f1f;
+`;
+
+export const buttonHoverColor = css`
+  background-color: #333;
+`;
+
+export const neutralTextColor = css`
+  color: #888;
+`;
+
+export const buttonBorderRadius = css`
+  border-radius: 10px;
+`;
+
+export const imageBorderRadius = css`
+  border-radius: 20px;
+`;
