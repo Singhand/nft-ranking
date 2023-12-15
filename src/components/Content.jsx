ba2f83ccf8ad13acc76aab5cc8585234eadfc474
KSH <rla_tmdgus@ssafy.com>
2023-12-16 00:27:55 +0900

    add: Content

---
+++ src/components/Content.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-top: 30px;
+  gap: 10px;
+`;
+
+const ContentTitle = styled.div`
+  font-size: 24px;
+`;
+
+export default function Content({ children, title }) {
+  return (
+    <ContentWrapper>
+      <ContentTitle>{title}</ContentTitle>
+      {children}
+    </ContentWrapper>
+  );
+}
