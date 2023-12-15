faa6bcfb9b56358c4be1ff28c4cfb86d42c1d975
KSH <rla_tmdgus@ssafy.com>
2023-12-16 00:37:02 +0900

    feat: HotPeople

---
+++ src/components/HotPeople.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import { imageBorderRadius } from "../style/common";
+import Content from "./Content";
+
+const IMG_SIZE = "200px";
+
+const RowCtn = styled.div`
+  display: flex;
+  width: 100%;
+  overflow: auto;
+  gap: 20px;
+`;
+
+const Image = styled.img`
+  width: ${IMG_SIZE};
+  height: ${IMG_SIZE};
+  object-fit: cover;
+  ${imageBorderRadius}
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+const Name = styled.div`
+  font-size: 22px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const ElementBox = styled.div`
+  max-width: ${IMG_SIZE};
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export default function HotPeople() {
+  return (
+    <Content title={"HOT"}>
+      <RowCtn>
+        <Element></Element>
+      </RowCtn>
+    </Content>
+  );
+}
+
+function Element({ url, name, id }) {
+  return (
+    <ElementBox>
+      <Image src="https://picsum.photos/200"></Image>
+      <Name>Camila</Name>
+    </ElementBox>
+  );
+}
