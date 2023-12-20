bf9bd25136af095bab2bc600518a056985ae0851
KSH <rla_tmdgus@ssafy.com>
2023-12-21 02:27:34 +0900

    add: 마이페이지 라우트

--- src/components/RoutesWithAnimation.jsx
+++ src/components/RoutesWithAnimation.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import Detail from "../routes/Detail";
 import Home from "../routes/Home";
+import MyProfile from "../routes/MyProfile";
 
 export default function RoutesWithAnimation() {
   const location = useLocation();
@@ -8,6 +9,7 @@ export default function RoutesWithAnimation() {
     <Routes location={location} key={location.key}>
       <Route path="/" element={<Home></Home>}></Route>
       <Route path="/detail" element={<Detail></Detail>}></Route>
+      <Route path="/my" element={<MyProfile></MyProfile>}></Route>
     </Routes>
   );
 }
---
+++ src/routes/MyProfile.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Wrapper } from "../components/Wrapper";
+import styled from "styled-components";
+
+export default function MyProfile() {
+  return <Wrapper>MyProfile</Wrapper>;
+}
